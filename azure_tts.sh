650bc222dfb37e51324d78f95f2eadc80cb6a7d4
Will B Chang <will.b.chang@gmail.com>
2023-08-31 11:34:44 +0800

    add options and default value for region, format, lang and voice

--- azure_tts.sh
+++ azure_tts.sh
@@ -3,8 +3,11 @@
 # name: Azure TTS
 # icon: symbol:message.and.waveform
 # options:
-# - { identifier: region, label: Azure Region, type: string, description: "Region for Speech Service" }
+# - { identifier: region, label: Azure Region, type: string, default value: eastus, description: "Region for Speech Service" }
 # - { identifier: key, label: Azure Subscription Key, type: string, description: "Your Speech resource key" }
+# - { identifier: format, label: Microsoft Output Format, type: string, default value: audio-16khz-32kbitrate-mono-mp3, description: "Audio output format" }
+# - { identifier: lang, label: Language, type: string, default value: en-US, description: "Language for text" }
+# - { identifier: voice, label: Voice Name, type: string, default value: en-US-JennyMultilingualNeural, description: "The name of voice" }
 
 # Create a temporary audio file
 temp_audio_file=$(mktemp)
@@ -13,9 +16,9 @@ temp_audio_file=$(mktemp)
 curl -X POST "https://${POPCLIP_OPTION_REGION}.tts.speech.microsoft.com/cognitiveservices/v1" \
      -H "Ocp-Apim-Subscription-Key: ${POPCLIP_OPTION_KEY}" \
      -H "Content-Type: application/ssml+xml" \
-     -H "X-Microsoft-OutputFormat: audio-16khz-32kbitrate-mono-mp3" \
-     -d "<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xml:lang=\"en-US\">
-    <voice name=\"en-US-JennyMultilingualNeural\">
+     -H "X-Microsoft-OutputFormat: ${POPCLIP_OPTION_FORMAT}" \
+     -d "<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xml:lang=\"${POPCLIP_OPTION_LANG}\">
+    <voice name=\"${POPCLIP_OPTION_VOICE}\">
         $POPCLIP_TEXT
     </voice>
 </speak>" -so "$temp_audio_file"
